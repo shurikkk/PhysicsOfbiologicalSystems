20ee8db6b83b43334c84b1b7a8c3e06001e2195e
shurikkk <leibenzon@gmail.com>
2022-02-07 01:34:27 +0200

    Final

--- Final/q6_2_prepare_simulation.py
+++ Final/q6_2_prepare_simulation.py
@@ -28,32 +28,31 @@ def mutation_rate_func_2(alpha_g, wt_number, mutation_number):
 
 
 if __name__ == '__main__':
-    function_i = 1
     functions = [mutation_rate_func_1, mutation_rate_func_2]
-    rate_func = functions[function_i]
+    for function_i, rate_func in enumerate(functions):
 
-    number_of_mutations = np.zeros(q6_framework.c)
-    minimal_mutation_generation = np.zeros(q6_framework.c)
-    for i in range(q6_framework.c):
-        start = timer()
+        number_of_mutations = np.zeros(q6_framework.c)
+        minimal_mutation_generation = np.zeros(q6_framework.c)
+        for i in range(q6_framework.c):
+            start = timer()
 
-        with Pool(8) as p:
-            simulation_res = p.map(q6_framework.simulate_single_branch,
-                                   [rate_func for j in range(q6_framework.n0)])
-            count = sum(x[0] for x in simulation_res)
-            min_g = min(x[1] for x in simulation_res)
-            number_of_mutations[i] = count
-            minimal_mutation_generation[i] = min_g
-        print(i, (count, min_g))
+            with Pool(8) as p:
+                simulation_res = p.map(q6_framework.simulate_single_branch,
+                                       [rate_func for j in range(q6_framework.n0)])
+                count = sum(x[0] for x in simulation_res)
+                min_g = min(x[1] for x in simulation_res)
+                number_of_mutations[i] = count
+                minimal_mutation_generation[i] = min_g
+            print(i, (count, min_g))
 
-        # count, min_g = q6_framework.simulate_single_branch()
-        # number_of_mutations[ii][i] = count
-        # minimal_mutation_generation[ii][i] = min_g
-        # print(ii, i, (count, min_g))
+            # count, min_g = q6_framework.simulate_single_branch()
+            # number_of_mutations[ii][i] = count
+            # minimal_mutation_generation[ii][i] = min_g
+            # print(ii, i, (count, min_g))
 
-        end = timer()
-        print(end - start)
+            end = timer()
+            print(end - start)
 
-    # print(res)
-    np.save('mutations_{i}'.format(i=function_i), number_of_mutations)
-    np.save('minimal_generation_{i}'.format(i=function_i), minimal_mutation_generation)
+        # print(res)
+        np.save('mutations_{i}'.format(i=function_i), number_of_mutations)
+        np.save('minimal_generation_{i}'.format(i=function_i), minimal_mutation_generation)
